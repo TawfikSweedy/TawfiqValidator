d37b49d32aa977914d71f244d3640d11e2f0aa15
tawfik_sweedy@2grand.net <tawfik_sweedy@2grand.net>
2022-11-20 00:45:11 +0200

    Initial Commit

---
+++ TawfiqValidator/TawfiqValidator.h
@@ -0,0 +1,18 @@
+//
+//  TawfiqValidator.h
+//  TawfiqValidator
+//
+//  Created by Tawfik Sweedy✌️ on 11/20/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TawfiqValidator.
+FOUNDATION_EXPORT double TawfiqValidatorVersionNumber;
+
+//! Project version string for TawfiqValidator.
+FOUNDATION_EXPORT const unsigned char TawfiqValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TawfiqValidator/PublicHeader.h>
+
+
